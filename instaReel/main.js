37cd4a5886f19bf86e2c003d102926154c0e3552
ShadowJod <nikhilmishra0187@gmail.com>
2025-11-27 21:47:36 +0530

    insta like and reveal effect added

--- instaReel/main.js
+++ instaReel/main.js
@@ -1,120 +1,141 @@
 const reelsData = [
   {
-    profileImg: "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    profileImg:
+      "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     username: "nikhil_codes",
-    videoSrc : "vid1.m",
+    videoSrc: "videos/vid1.mp4",
     caption: "Learning React everyday! 💻🔥",
     bgImage: "https://source.unsplash.com/random/800x1200/?coding,technology",
     likeCount: 1200,
+    isliked : true,
     shareCount: 150,
     commentCount: 85,
-    follow: false
+    follow: false,
   },
   {
-    profileImg: "https://images.unsplash.com/photo-1516726817505-f5ed825624d8?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    profileImg:
+      "https://images.unsplash.com/photo-1516726817505-f5ed825624d8?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     username: "tech_girl",
-    videoSrc : "vid2.mp4",
+    videoSrc: "videos/vid2.mp4",
     caption: "Frontend vibes ✨ #HTML #CSS #JS",
     bgImage: "https://source.unsplash.com/random/800x1200/?frontend,laptop",
     likeCount: 980,
+    isliked : false,
     shareCount: 95,
     commentCount: 60,
-    follow: true
+    follow: true,
   },
   {
-    profileImg: "https://images.unsplash.com/photo-1526510747491-58f928ec870f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    profileImg:
+      "https://images.unsplash.com/photo-1526510747491-58f928ec870f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     username: "anime_world",
-    videoSrc : "vid3.mp4",
+    videoSrc: "videos/vid3.mp4",
     caption: "Naruto is ❤️ Believe it! 🍥",
     bgImage: "https://source.unsplash.com/random/800x1200/?anime,art",
     likeCount: 4500,
+    isliked : false,
     shareCount: 320,
     commentCount: 210,
-    follow: false
+    follow: false,
   },
   {
-    profileImg: "https://plus.unsplash.com/premium_photo-1707932495000-5748b915e4f2?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    profileImg:
+      "https://plus.unsplash.com/premium_photo-1707932495000-5748b915e4f2?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     username: "fitness_mania",
-    videoSrc : "vid4.mp4",
+    videoSrc: "videos/vid4.mp4",
     caption: "Gym day — no excuses 💪🔥",
     bgImage: "https://source.unsplash.com/random/800x1200/?gym,fitness",
     likeCount: 2200,
+    isliked : false,
     shareCount: 180,
     commentCount: 140,
-    follow: false
+    follow: false,
   },
   {
-    profileImg: "https://plus.unsplash.com/premium_photo-1664875849194-0adbac28529f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    profileImg:
+      "https://plus.unsplash.com/premium_photo-1664875849194-0adbac28529f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     username: "travel_buddy",
-    videoSrc : "vid5.mp4",
+    videoSrc: "videos/vid5.mp4",
     caption: "Exploring new places 🌍✈️",
     bgImage: "https://source.unsplash.com/random/800x1200/?travel,nature",
     likeCount: 3500,
+    isliked : false,
     shareCount: 400,
     commentCount: 260,
-    follow: true
+    follow: true,
   },
   {
-    profileImg: "https://images.unsplash.com/photo-1512310604669-443f26c35f52?q=80&w=734&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    profileImg:
+      "https://images.unsplash.com/photo-1512310604669-443f26c35f52?q=80&w=734&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     username: "foodie_life",
-    videoSrc : "vid6.mp4",
+    videoSrc: "videos/vid6.mp4",
     caption: "Pasta night 🍝❤️",
     bgImage: "https://source.unsplash.com/random/800x1200/?food,restaurant",
     likeCount: 1900,
+    isliked : false,
     shareCount: 120,
     commentCount: 75,
-    follow: false
+    follow: false,
   },
   {
-    profileImg: "https://images.unsplash.com/photo-1512310604669-443f26c35f52?q=80&w=734&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    profileImg:
+      "https://images.unsplash.com/photo-1512310604669-443f26c35f52?q=80&w=734&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     username: "coder_boy",
-    videoSrc : "vid7.mp4",
+    videoSrc: "videos/vid7.mp4",
     caption: "Debugging = Brain Exercise 🧠💡",
     bgImage: "https://source.unsplash.com/random/800x1200/?code,monitor",
     likeCount: 2500,
+    isliked : false,
     shareCount: 160,
     commentCount: 110,
-    follow: true
+    follow: true,
   },
   {
-    profileImg: "https://images.unsplash.com/photo-1512310604669-443f26c35f52?q=80&w=734&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D  ",
+    profileImg:
+      "https://images.unsplash.com/photo-1512310604669-443f26c35f52?q=80&w=734&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D  ",
     username: "dance_vibes",
-    videoSrc : "vid8.mp4",
+    videoSrc: "videos/vid8.mp4",
     caption: "Dance like nobody’s watching 💃✨",
     bgImage: "https://source.unsplash.com/random/800x1200/?dance,stage",
     likeCount: 4100,
+    isliked : false,
     shareCount: 350,
     commentCount: 300,
-    follow: false
+    follow: false,
   },
   {
-    profileImg: "https://images.unsplash.com/photo-1512310604669-443f26c35f52?q=80&w=734&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    profileImg:
+      "https://images.unsplash.com/photo-1512310604669-443f26c35f52?q=80&w=734&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     username: "pet_lovers",
-    videoSrc : "vid9.mp4",
+    videoSrc: "videos/vid9.mp4",
     caption: "My dog = My happiness 🐶❤️",
     bgImage: "https://source.unsplash.com/random/800x1200/?dog,cute",
     likeCount: 2800,
+    isliked : false,
     shareCount: 200,
     commentCount: 130,
-    follow: false
+    follow: false,
   },
   {
-    profileImg: "https://plus.unsplash.com/premium_photo-1664464229692-44046bfd6b7d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    profileImg:
+      "https://plus.unsplash.com/premium_photo-1664464229692-44046bfd6b7d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     username: "motivator_daily",
-    videoSrc : "vid10.mp4",
+    videoSrc: "videos/vid10.mp4",
     caption: "Stay consistent, success will follow 🚀🔥",
     bgImage: "https://source.unsplash.com/random/800x1200/?motivation,success",
     likeCount: 5200,
-    shareCount: 480,
-    commentCount: 350,
-    follow: true
-  }
+    isnCount: 350,
+    follow: true,
+  },
 ];
+let allreel = document.querySelector("#allReels");
+let button =  document.querySelector("button")
 
-let count = ''
-reelsData.forEach((e)=>{
-    count+=`<div class="reel">
-          <video muted autoplay loop src="${e.videoSrc}">
+function addData(){
+    let count = "";
+  reelsData.forEach((e, idx) => {
+    count += `<div class="reel">
+          <video muted     .[\] autoplay loop src="${e.videoSrc}">
 
         </video>
 
@@ -136,8 +157,8 @@ reelsData.forEach((e)=>{
           </div>
 
           <div id="alllogo">
-            <div id="likelogo">
-              <i class="ri-heart-line"></i>
+            <div id = "${idx}" class="likelogo">
+            ${e.isliked?`<i class="ri-heart-fill"></i>`:`<i class="ri-heart-line"></i>`}
               <p>${e.likeCount}</p>
             </div>
 
@@ -150,10 +171,32 @@ reelsData.forEach((e)=>{
               <i class="ri-share-forward-fill"></i>
               <p>${e.shareCount}</p>
             </div>
+
+            <div id="menulogo">
+              <i class="ri-more-2-fill"></i>
+            </div>
           </div>
-        </div>`
-    
+        </div>`;
+  });
+
+  let allreel = document.querySelector("#allReels");
+  allreel.innerHTML = count;
+}
+
+addData();
+
+allreel.addEventListener("click", (e)=>{
+  if(reelsData[e.target.id].isliked){
+    reelsData[e.target.id].likeCount--;
+    reelsData[e.target.id].isliked = false;
+  }
+  else{
+    reelsData[e.target.id].isliked = true;
+    reelsData[e.target.id].likeCount++;
+  }
+  addData();
+  
 })
 
-let allreel = document.querySelector("#allReels");
-allreel.innerHTML = count;
+
+
