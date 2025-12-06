f3fc703d52322c5d3e09ae9d7b9f61ee4fda9683
ShadowJod <nikhilmishra0187@gmail.com>
2025-12-06 23:05:55 +0530

    Up Down button addded

---
+++ upDown/main.js
@@ -0,0 +1,20 @@
+let upBtn = document.querySelector('#up');
+let downBtn = document.querySelector('#down');
+
+let cardContainer = document.querySelector('.cards');
+let cards = document.querySelectorAll('.card');
+
+
+console.dir(upBtn);
+console.dir(downBtn);
+console.dir(cardContainer);
+
+
+downBtn.addEventListener('click', ()=>{
+    
+})
+
+
+
+
+
