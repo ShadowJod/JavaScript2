c1fbbe5c428f431a7599115fefd3a14573004853
ShadowJod <nikhilmishra0187@gmail.com>
2025-12-07 19:58:22 +0530

    up down complete

--- upDown/main.js
+++ upDown/main.js
@@ -1,20 +1,24 @@
-let upBtn = document.querySelector('#up');
-let downBtn = document.querySelector('#down');
+let upBtn = document.querySelector("#up");
+let downBtn = document.querySelector("#down");
 
-let cardContainer = document.querySelector('.cards');
-let cards = document.querySelectorAll('.card');
+let cardContainer = document.querySelector(".cards");
+let card = document.querySelector(".card");
 
+let bttn = document.querySelector("#bttn");
+let currentCard = card;
+let z = 3;
 
-console.dir(upBtn);
-console.dir(downBtn);
-console.dir(cardContainer);
-
-
-downBtn.addEventListener('click', ()=>{
-    
-})
-
-
-
-
-
+bttn.addEventListener("click", (e) => {
+  let ele = e.target.id;
+  if (ele === "up") {
+    let prev = currentCard.previousElementSibling;
+    if (prev === null) return;
+    currentCard = prev;
+    currentCard.style.zIndex = ++z;
+  } else {
+    let next = currentCard.nextElementSibling;
+    if (next === null) return;
+    currentCard = next;
+    currentCard.style.zIndex = ++z;
+  }
+});
